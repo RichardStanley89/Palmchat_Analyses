966354c923312f828d37558ccd55e79512c9eaf5
Rick Stanley <rickstanley89@gmail.com>
2026-02-03 10:57:51 -0500

    Latest models

--- notebooks/20251121_JaviModel.R
+++ notebooks/20251121_JaviModel.R
@@ -1,55 +1,9 @@
 # Load in behavior data
 
-standardize_strings <- function(strings) {
-  sapply(strings, function(s) {
-    # Extract components using regex
-    # Handle different patterns
-    
-    # Pattern 1: Day_Month_Year_PTZ_TOD
-    if (grepl("^\\d+_[A-Za-z]+_\\d{4}_PTZ", s)) {
-      parts <- str_match(s, "^(\\d+)_([A-Za-z]+)_(\\d{4})_(PTZ[^_]+)_(.+)$")
-      day <- parts[2]
-      month <- parts[3]
-      year <- parts[4]
-      cam <- parts[5]
-      tod <- parts[6]
-    }
-    # Pattern 2: PTZ_Month_Day_Year_TOD
-    else if (grepl("^PTZ", s)) {
-      parts <- str_match(s, "^(PTZ[^_]+)_([A-Za-z]+)_?(\\d+)_?(\\d{4})_(.+)$")
-      cam <- parts[2]
-      month <- parts[3]
-      day <- parts[4]
-      year <- parts[5]
-      tod <- parts[6]
-    }
-    else {
-      return(NA)
-    }
-    
-    # Standardize month (abbreviate to 3 letters, capitalize first)
-    month <- str_replace(month, "June", "Jun")
-    month <- str_replace(month, "July", "July")
-    month <- str_replace(month, "August", "August")
-    month <- str_replace(month, "Junr", "Jun")
-    
-    # Standardize TOD
-    tod <- str_replace(tod, "Morn?", "Mor")
-    tod <- str_replace(tod, "^Ev.*", "Ev")
-    
-    # Return standardized format
-    paste(day, month, year, cam, tod, sep = "_")
-  }, USE.NAMES = FALSE)
-}
 
-behavior_data <- read.csv("processed_data/Palmchat_Behavior_Data_Nov_15_2025.csv")
-
+library(dplyr)
 library(tidyverse)
 
-behavior_data %>% 
-  select(Observation.id) %>% 
-  filter(str_detect(Observation.id, "_Morn")) %>% 
-  mutate(Observation.id = str_replace(Observation.id, "_Morn", "_Mor"))
 
 
 # Focus on specific behaviors for each of the models
@@ -63,72 +17,252 @@ behavior_data %>%
 # random effect has to be the nest entrance
 # Count - number of observations per individual, and per nest entrance
 
-glimpse(behavior_data)
+
+D <- read.csv("processed_data/Behavior_data.csv")
 
 
-A <- behavior_data %>% 
-  select(
-    Observation.id,
-    Observation.date,
-    Media.duration..s.,
-    Subject,
-    Behavior,
-    Behavioral.category,
-    Modifier..1,
-    Behavior.type,
-    Start..s.,
-    Stop..s.
-  )
+# Below we will make data frames with each of the behavioral variables of interest.
+# There are a bunch of them.
 
+# select "arrive" behavior as response variable
 
-split_values <- strsplit(A$Media.duration..s., split =";")
-split_values <- sapply(split_values, as.numeric)
-A <- A %>% mutate(Duration = sapply(split_values, sum))
-
-
-attributes <- read.csv("rawdata/PalmchatBandingDataAll.csv")
-
-bird_ids <- attributes$Colors
-
-B <- A %>% 
-  filter(Subject %in% bird_ids)
-
-Camera_data <- read.csv("rawdata/FoscamCameraScheduleCodedNew.csv", na.strings = c("", "NA"))
-
-unique(Camera_data$Observation.id)
-unique(standardize_strings(B$Observation.id))
-
-
-C <- B %>% 
-  select(-Media.duration..s.) %>% 
-  mutate(Observation.id = standardize_strings(Observation.id))
-
-D <- Camera_data %>% 
-  select(Observation.id, Nest, Nest.Entrance) %>% 
-  drop_na(Observation.id) %>% 
-  left_join(C, by = join_by(Observation.id)) %>% 
-  drop_na(Observation.id, Nest.Entrance, Subject)
-
-
-D %>% 
-  count(Nest.Entrance, Subject)
-
-F <- D %>% 
+S <- D %>% 
   filter(Behavior == "arrive") %>% 
   count(Observation.id, Nest.Entrance, Subject, Duration) %>% 
-  mutate(log_effort = log(Duration)) %>% 
+  mutate(log_effort = log(Duration)) %>%
+  # mutate(log_effort = Duration) %>% 
   mutate(tod = str_extract(Observation.id, "(Mor|Ev)$"))
 
-hist(F$n)
+
+
+
+hist(S$n)
+
+# select "enter" behavior as response variable
+
+S <- D %>%
+  filter(Behavior == "enter") %>% 
+  count(Observation.id, Nest.Entrance, Subject, Duration) %>% 
+  mutate(log_effort = log(Duration)) %>%
+  # mutate(log_effort = Duration) %>% 
+  mutate(tod = str_extract(Observation.id, "(Mor|Ev)$"))
+
+
+# select "build" behavior as response variable
+
+S <- D %>% 
+  filter(Behavior == "build") %>% 
+  count(Observation.id, Nest.Entrance, Subject, Duration) %>% 
+  mutate(log_effort = log(Duration)) %>%
+  # mutate(log_effort = Duration) %>% 
+  mutate(tod = str_extract(Observation.id, "(Mor|Ev)$"))
+
+# Select build DURATION as response:
+
+behaviorfrequency <- D %>% group_by(D$Observation.id, D$Subject) %>% 
+  filter(Behavior == "build") %>%
+summarise(duration_behavior = sum(Duration..s.), count = n(),)
+
+hist(log(behaviorfrequency$duration_behavior))
+
+## try a log transformation??
+
+# behaviorfrequency <- D %>% group_by(D$Observation.id, D$Subject) %>% 
+#  filter(Behavior == "build") %>%
+#  summarise(duration_behavior = log(sum(Duration..s.)), count = n(),)
+
+S <- S %>% mutate(duration_behavior = behaviorfrequency$duration_behavior)
+
+# Select "visible" as a response:
+
+S <- D %>% 
+  filter(Behavior == "visible") %>% 
+  count(Observation.id, Nest.Entrance, Subject, Duration) %>% 
+  mutate(log_effort = log(Duration)) %>%
+  # mutate(log_effort = Duration) %>% 
+  mutate(tod = str_extract(Observation.id, "(Mor|Ev)$"))
+
+# Select "duration visible" as a response:
+
+behaviorfrequency <- D %>% group_by(D$Observation.id, D$Subject) %>% 
+  filter(Behavior == "visible") %>%
+  summarise(duration_behavior = sum(Duration..s.), count = n(),)
+
+S <- S %>% mutate(duration_behavior = behaviorfrequency$duration_behavior)
+
+
+
+
+# Select "BringFood" as a response:
+
+S <- D %>% 
+  filter(BringFood == "BringFood") %>% 
+  count(Observation.id, Nest.Entrance, Subject, Duration) %>% 
+  mutate(log_effort = log(Duration)) %>%
+  # mutate(log_effort = Duration) %>% 
+  mutate(tod = str_extract(Observation.id, "(Mor|Ev)$"))
+
+
+# create binary response: helper vs non-helper
+
+helpers <- S %>% mutate(helper = 1)
+
+helpers <- data.frame(helpers$Subject, helpers$helper)
+
+helpers <- helpers %>% rename(Subject = helpers.Subject, helper = helpers.helper)
+
+joined <- left_join(D, helpers, by = 'Subject')
+
+joined <- joined %>% mutate(helper = replace_na(helper, 0))
+
+                   
+S <- joined %>% 
+  count(Observation.id, Nest.Entrance, Subject, Duration, helper) %>% 
+  mutate(log_effort = log(Duration)) %>%
+  # mutate(log_effort = Duration.x) %>% 
+  mutate(tod = str_extract(Observation.id, "(Mor|Ev)$"))
+
+
+#########
+
+
+# Now we can run the models with any of the above selections....
+
+# join filtered data with attributes:
+
+attributes_clean2 <- read.csv("processed_data/attributes_clean2.csv")
+
+Joined <- inner_join(attributes_clean2, S, by = "Subject")
+
+Joined <- Joined %>% mutate(molecular_sex = as.factor(Joined$molecular_sex))
+
+Joined <- Joined %>% mutate(age = as.factor(Joined$age))
+
+str(Joined)
+
 
 library(lme4)
 
+# Does time of day influence the "point" behavior?
+
 model <- glmer(n ~ tod + (1|Subject) + offset(log_effort),
-      data = F,
+      data = S,
       family = poisson)
+
+
+summary(model)
+
+
+# Does time of day influence the "state" behavior?
+
+model <- lmer(log(duration_behavior) ~ tod + (1|Subject) + offset(log_effort),
+               data = Joined)
+
+model <- glmer(duration_behavior ~ tod + (1|Subject) + offset(log_effort),
+              data = Joined,
+              family = Gamma(link = "inverse"))
+
+model
+
+summary(model)
+
+?glmer
+
+
+# remove NAs to prepare molecular sex column:
+
+
+Joined <- Joined %>%
+     mutate(molecular_sex = na_if(molecular_sex, ""))
+
+Joined <- Joined %>% drop_na(molecular_sex)
+
+
+# Does sex influence the "point" behavior?
+
+
+
+
+model <- glmer(n ~ molecular_sex + tod + (1|Subject) + offset(log_effort),
+               data = Joined,
+               family = poisson)
+
+
+summary(model)
+
+exp(0.4645)
+
+
+
+# Does sex influence the state behavior?
+
+
+model <- glmer(duration_behavior ~ molecular_sex + (1|Subject) + offset(log_effort),
+               data = Joined,
+               family = gaussian)
+
+
+
+
+# Does sex influence the binary category?
+
+hist(S$helper)
+
+
+model <- glmer(helper ~ molecular_sex + (1|Subject) + offset(log_effort),
+               data = Joined,
+               family = binomial(link = "logit"))
+
+table(S$helper)
+
+# Let's prepare the age variable:
+
+# remove NAs to prepare age column (good bit of data loss):
+
+
+Joined <- Joined %>%
+  mutate(age = na_if(age, ""))
+
+Joined <- Joined %>% drop_na(age)
+
+
+# if we want, we could simplify "age" to "adult and "juvenile"
+# Joined <- Joined %>% mutate(age2 = case_when(age == "FCF" ~ "Immature",
+       #                                      age == "FAJ" ~ "Adult",
+       #                                      age == "juvenile" ~ "Immature",
+       #                                      age == "DCB" ~ "Adult",
+         #                                    TRUE ~ "OTHER"))
+
+
+
+# Does age influence the point behavior?
+
+
+model <- glmer(n ~ age + (1|Subject) + offset(log_effort),
+               data = Joined,
+               family = poisson)
+
+summary(model)
+
+
+# Does age influence the state behavior?
+
+model <- glmer(duration_behavior ~ age + (1|Subject) + offset(log_effort),
+               data = Joined,
+               family = gaussian)
+
+
 
 # Check significance
 summary(model)
+
+
+install.packages("sjPlot")
+library(sjPlot)
+
+tab_model(model)
+
+
 
 library(DHARMa)
 
